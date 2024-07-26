b183beb795d733a9424433746cdcc2757bb9bd41
fazzy12 <kaluifeanyi00@gmail.com>
2024-07-26 14:38:15 +0100

    config: adjust config settings

--- app/config.py
+++ app/config.py
@@ -1,7 +1,6 @@
-# backend/config.py
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres.mrhazjgjbxnssyvklcwr:YOUR-PASSWORD@aws-0-eu-central-1.pooler.supabase.com:6543/postgres'
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.urandom(24)
